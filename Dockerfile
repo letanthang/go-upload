8f47ec5a4cbf3861249a48d329fc1c910690fc7b
Le Tan Thang <letanthang@gmail.com>
2019-04-26 11:31:11 +0700

    init code

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM golang:1.11-alpine as builder
+WORKDIR /go/src/github.com/go-upload/
+COPY . /go/src/github.com/go-upload/
+RUN go build -o ./dist/go-upload
+
+FROM alpine:3.5
+RUN apk add --update ca-certificates
+RUN apk add --no-cache tzdata && \
+  cp -f /usr/share/zoneinfo/Asia/Ho_Chi_Minh /etc/localtime && \
+  apk del tzdata
+
+WORKDIR /app
+COPY ./config/go-upload.yaml /var/app/
+COPY ./config/go-upload.yaml /
+COPY ./config/fine-iterator-231706-04d01216bb7a.json /var/app/
+COPY --from=builder go/src/github.com/go-upload/dist/go-upload .
+ENV GOOGLE_APPLICATION_CREDENTIALS /var/app/ghn-logistic-acb03f8ecff8.json
+EXPOSE 9090
+ENTRYPOINT ["./go-upload"]
---
+++ config/config.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+type Schema struct {
+	Db struct {
+		Host     string `mapstructure:"host"`
+		User     string `mapstructure:"user"`
+		Name     string `mapstructure:"name"`
+		Password string `mapstructure:"password"`
+		Debug    bool   `mapstructure:"debug"`
+	} `mapstructure:"go_postgres_database"`
+
+	Paging struct {
+		Limit int `mapstructure:"limit"`
+	} `mapstructure:"paging"`
+
+	Encryption struct {
+		OIDKey           string `mapstructure:"oid_key"`
+		JWTSecret        string `mapstructure:"jwt_secret"`
+		JWTSecretPartner string `mapstructure:"jwt_secret_partner"`
+		JWTExp           int    `mapstructure:"jwt_exp"`
+		JWTPol           string `mapstructure:"jwt_pol"`
+	} `mapstructure:"encryption"`
+}
+
+var Config Schema
+
+func init() {
+	config := viper.New()
+	config.SetConfigName("go-upload")
+	config.AddConfigPath(".")          // Look for config in current directory
+	config.AddConfigPath("config/")    // Optionally look for config in the working directory.
+	config.AddConfigPath("../config/") // Look for config needed for tests.
+	config.AddConfigPath("../")        // Look for config needed for tests.
+
+	config.SetEnvKeyReplacer(strings.NewReplacer(".", "__"))
+	config.AutomaticEnv()
+
+	err := config.ReadInConfig() // Find and read the config file
+	if err != nil {              // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+	err = config.Unmarshal(&Config)
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+}
